f4f76fe64b2ae538775d73822463c7fc86709608
Armin Grodon <me@armingrodon.de>
2021-01-22 14:44:42 +0100

    Add first unit tests

--- src/json.rs
+++ src/json.rs
@@ -12,10 +12,19 @@ pub struct Account {
     pub roles: Vec<String>,
 }
 
-pub fn read_config(path: &PathBuf) -> Result<Vec<Account>, io::Error> {
+fn read_file(path: &PathBuf) -> Result<String, io::Error> {
     let mut file = File::open(path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    let res: Vec<Account> = serde_json::from_str(&mut contents)?;
+    Ok(contents)
+}
+
+fn parse_json(contents: &String) -> Result<Vec<Account>, serde_json::Error> {
+    serde_json::from_str::<Vec<Account>>(&contents)
+}
+
+pub fn read_config(path: &PathBuf) -> Result<Vec<Account>, io::Error> {
+    let contents = read_file(&path)?;
+    let res = parse_json(&contents)?;
     Ok(res)
 }
--- src/shell.rs
+++ src/shell.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+#[derive(PartialEq, Debug)]
 pub enum Shell {
     Fish,
     Bash,
@@ -26,5 +27,52 @@ pub fn export_string(shell: &Shell, var: &str, val: &String) -> String {
     match shell {
         Shell::Fish => format!("set -gx {} {};", var, val),
         Shell::Bash => format!("export {}={}", var, val),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reading_from_string() {
+        assert_eq!(Shell::Bash, from_str("bash"));
+        assert_eq!(Shell::Fish, from_str("fish"));
+        assert_eq!(Shell::Bash, from_str("zsh")); // TODO: replace with quickcheck
+    }
+
+    #[test]
+    fn reading_from_env() {
+        env::set_var("SHELL", "/usr/bin/fish");
+        assert_eq!(Shell::Fish, get_shell(&None));
+        env::set_var("SHELL", "/usr/bin/bash");
+        assert_eq!(Shell::Bash, get_shell(&None));
+    }
+
+    #[test]
+    fn prefer_preselect_over_env() {
+        env::set_var("SHELL", "/usr/bin/fish");
+        assert_eq!(Shell::Bash, get_shell(&Some(String::from("bash"))));
+    }
+
+    #[test]
+    fn string_formatting() {
+        // TODO: generalize test cases for different shells
+        assert_eq!(
+            export_string(&Shell::Fish, "", &String::from("")),
+            "set -gx  ;"
+        ); // TODO: this should panic
+        assert_eq!(
+            export_string(&Shell::Fish, "FOO", &String::from("bar")),
+            "set -gx FOO bar;"
+        );
+        assert_eq!(
+            export_string(&Shell::Bash, "", &String::from("")),
+            "export ="
+        ); // TODO: this should panic
+        assert_eq!(
+            export_string(&Shell::Bash, "FOO", &String::from("bar")),
+            "export FOO=bar"
+        );
     }
 }
